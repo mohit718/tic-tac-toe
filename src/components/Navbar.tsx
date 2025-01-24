512e1dac459d9c4f299188df889fc6f75cb2065e
Mohit Soni <mohit18.official@gmail.com>
2025-01-24 12:59:09 +0530

    Github link updated

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,7 +6,12 @@ const Navbar: React.FC = () => {
         <nav className="p-4 flex justify-between items-center w-full">
             <a href="#"><h1 className="lg:text-4xl text-2xl font-bold font-display">Tic-Tac-Toe</h1></a>
             <div className="flex justify-end items-center">
-                <button className="rounded-full mx-2 p-2 px-4 bg-primary text-light font-bold flex items-center gap-2 hover:cursor-pointer">Github <span className="md:text-3xl text-lg"><FaGithub /></span></button>
+                <button 
+                    className="rounded-full mx-2 p-2 px-4 bg-primary text-light font-bold flex items-center gap-2 hover:cursor-pointer" 
+                    onClick={() => window.open('https://github.com/mohit718/tic-tac-toe', '_blank')}
+                >
+                    Github <span className="md:text-3xl text-lg"><FaGithub /></span>
+                </button>
             </div>
         </nav>
     );
